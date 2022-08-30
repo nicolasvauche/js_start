476b0c0802dcbbc43fcddfe1d5d1dfbe5a599990
Nicolas vauché <hello@nicolasvauche.net>
2022-08-30 16:01:52 +0200

    Fetch JSON with JS

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
-// Pointer sur un élément du DOM
-const monBouton = document.getElementById('clickme')
-
-// Écouter l'évènement Click déclenché sur notre bouton
-monBouton.addEventListener('click', function () {
-    // Demander une saisie à l'utilisateur
-  const tonNom = window.prompt(
-    'Tu as cliqué petit malin, dis-moi ton nom maintenant'
-  )
-
-  if (tonNom !== '' && tonNom !== null) {
-    // Si la variable n'est pas vide et pas nulle non plus
-    const paragraphe = document.querySelector('#message')
-    paragraphe.innerText = 'Bonjour ' + tonNom
-  } else {
-    // Sinon
-    window.alert('Ben du coup salut')
-  }
-})
+// Envoyer une requete asynchrone avec JS
+fetch('data/users.json')
+  // Séparer les données de la réponse reçue
+  .then(response => response.json())
+  // Traiter les données reçues
+  .then(userData => {
+    const userInfos = document.getElementById('user')
+    userInfos.innerHTML = "<p><strong>Nom :</strong> " + userData.name + "</p>"
+    userInfos.innerHTML += "<p><strong>Email :</strong> " + userData.email + "</p>"
+    userInfos.innerHTML += "<p><strong>Mot de passe :</strong> " + userData.password + "</p>"
+  })
+  // Agir en cas d'erreur
+  .catch(e => {
+    console.log(e.message)
+  })
