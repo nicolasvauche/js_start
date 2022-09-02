3431ec5f0bef37a33921ed1065a8bdab09033111
Nicolas vauché <hello@nicolasvauche.net>
2022-09-02 11:22:04 +0200

    Fin

--- js/index.js
+++ js/index.js
@@ -3,11 +3,26 @@ fetch('data/users.json')
   // Séparer les données de la réponse reçue
   .then(response => response.json())
   // Traiter les données reçues
-  .then(userData => {
+  .then(users => {
     const userInfos = document.getElementById('user')
-    userInfos.innerHTML = "<p><strong>Nom :</strong> " + userData.name + "</p>"
-    userInfos.innerHTML += "<p><strong>Email :</strong> " + userData.email + "</p>"
-    userInfos.innerHTML += "<p><strong>Mot de passe :</strong> " + userData.password + "</p>"
+
+    // Boucler sur le tableau des utilisateurs
+    users.forEach(user => {
+      // Créer un div pour contenir l'utilisateur
+      const userContainer = document.createElement('div')
+      userContainer.classList.add('user')
+
+      for (const key in user) {
+        // Créer un paragraphe pour chaque propriété de l'utilisateur
+        const userInfoContainer = document.createElement('p')
+        userInfoContainer.innerHTML = '<strong>' + key.toUpperCase() + ' :</strong> ' + user[key]
+        // Ajouter la proopriété au conteneur
+        userContainer.appendChild(userInfoContainer)
+      }
+
+      // Ajouter le conteneur à notre DOM
+      userInfos.appendChild(userContainer)
+    })
   })
   // Agir en cas d'erreur
   .catch(e => {
