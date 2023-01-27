6c36120226b4bb7a0ad4bd9a9f282d32366bb1ca
Nicolas vauché <hello@nicolasvauche.net>
2023-01-27 09:30:39 +0100

    End

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const monBouton = document.getElementById('clickme')
 
 // Écouter l'évènement Click déclenché sur notre bouton
 monBouton.addEventListener('click', function () {
-    // Demander une saisie à l'utilisateur
+  // Demander une saisie à l'utilisateur
   const tonNom = window.prompt(
     'Tu as cliqué petit malin, dis-moi ton nom maintenant'
   )
@@ -11,9 +11,9 @@ monBouton.addEventListener('click', function () {
   if (tonNom !== '' && tonNom !== null) {
     // Si la variable n'est pas vide et pas nulle non plus
     const paragraphe = document.querySelector('#message')
-    paragraphe.innerText = 'Bonjour ' + tonNom
+    paragraphe.innerText = 'Bonjour ' + tonNom + ' :)'
   } else {
     // Sinon
-    window.alert('Ben du coup salut')
+    window.alert('Bon, ben du coup salut :(')
   }
 })
