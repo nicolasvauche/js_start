16bb465ef93e7c3563c18f95db9e54a90e25026f
Nicolas vauché <hello@nicolasvauche.net>
2023-02-01 18:03:11 +0100

    End

--- js/index.js
+++ js/index.js
@@ -3,27 +3,27 @@ fetch('data/users.json')
   // Séparer les données de la réponse reçue
   .then(response => response.json())
   // Traiter les données reçues
-  .then(users => {
-    const userInfos = document.getElementById('users')
+    .then(users => {
+      const userInfos = document.getElementById('users')
 
-    // Boucler sur le tableau des utilisateurs
-    users.forEach(user => {
-      // Créer un div pour contenir l'utilisateur
-      const userContainer = document.createElement('div')
-      userContainer.classList.add('user')
+      // Boucler sur le tableau des utilisateurs
+      users.forEach(user => {
+        // Créer un div pour contenir l'utilisateur
+        const userContainer = document.createElement('div')
+        userContainer.classList.add('user')
 
-      for (const key in user) {
-        // Créer un paragraphe pour chaque propriété de l'utilisateur
-        const userInfoContainer = document.createElement('p')
-        userInfoContainer.innerHTML =
-          '<strong>' + key.toUpperCase() + ':</strong> ' + user[key]
-        // Ajouter la proopriété au conteneur
-        userContainer.appendChild(userInfoContainer)
-      }
+        for (const key in user) {
+          // Créer un paragraphe pour chaque propriété de l'utilisateur
+          const userInfoContainer = document.createElement('p')
+          userInfoContainer.innerHTML =
+            '<strong>' + key.toUpperCase() + ':</strong> ' + user[key]
+          // Ajouter la proopriété au conteneur
+          userContainer.appendChild(userInfoContainer)
+        }
 
-      // Ajouter le conteneur à notre DOM
-      userInfos.appendChild(userContainer)
-    })
+        // Ajouter le conteneur à notre DOM
+        userInfos.appendChild(userContainer)
+      })
   })
   // Agir en cas d'erreur
   .catch(error => {
